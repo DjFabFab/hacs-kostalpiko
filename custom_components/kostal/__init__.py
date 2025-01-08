5edff586bdfacf394fbf9ba6f952f64dfac9d22d
djFabFab <68521979+DjFabFab@users.noreply.github.com>
2025-01-08 17:51:07 +0100

    fix: missing await deprecation warning

--- custom_components/kostal/__init__.py
+++ custom_components/kostal/__init__.py
@@ -49,6 +49,6 @@ async def async_setup(hass, config):
 async def async_setup_entry(hass:  HomeAssistant, entry: ConfigEntry):
     """Load the saved entities."""
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        await hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
